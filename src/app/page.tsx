a72b35b1b746367af30f5e3db864c4c1e73210e3
WinWinMaw4 <mawinwinmaw4@gmail.com>
2024-11-02 16:16:03 +0630

    fixed legacy problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ const page = () => {
   return (
     <div>
        <main className="text-7xl font-bold font-mono flex min-h-screen justify-center flex-col items-center p-4">
-       <AnimatedCounter from={0} to={100} />
+        <div>Win Win Maw Framer Motion</div>
+       <AnimatedCounter from={0} to={500} />
 
         </main>
     </div>
